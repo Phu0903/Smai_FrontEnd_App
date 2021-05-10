d72dc69406b49cef6c4d5f648b4ade4526b6d30a
Nguyễn Phú <71036328+Phu0903@users.noreply.github.com>
2021-05-11 00:18:05 +0700

    update

--- app/src/main/java/com/example/SmaiApp/HomeFragment.java
+++ app/src/main/java/com/example/SmaiApp/HomeFragment.java
@@ -1,19 +1,13 @@
-package com.example.quyengop;
+package com.example.SmaiApp;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -22,7 +16,7 @@ import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
 
-import static com.example.quyengop.Helper.setListViewHeightBasedOnChildren;
+import static com.example.SmaiApp.Helper.setListViewHeightBasedOnChildren;
 
 public class HomeFragment extends Fragment {
 
