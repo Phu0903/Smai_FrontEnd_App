206ea7aa5d077c2bcac815a105e280e96e2633d2
khang <anhkhang110@gmail.com>
2021-05-29 00:00:36 +0700

    connect ui

--- app/src/main/java/com/example/SmaiApp/NewpostType.java
+++ app/src/main/java/com/example/SmaiApp/NewpostType.java
@@ -2,13 +2,60 @@ package com.example.SmaiApp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NewpostType extends AppCompatActivity {
+
+    Button btnCanxindo, btnTangcongdong, btnTangnguoingheo, btnTangtuthien, btnQuyengop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newpost_type);
+
+        btnCanxindo = findViewById(R.id.btn_canxindo);
+        btnTangcongdong  = findViewById(R.id.btn_tangcongdong);
+        btnTangnguoingheo = findViewById(R.id.btn_tangnguoingheo);
+        btnTangtuthien = findViewById(R.id.btn_tangtuthien);
+        btnQuyengop = findViewById(R.id.btn_quyengop);
+
+        btnCanxindo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ConfirmAddress.class);
+                startActivity(intent);
+            }
+        });
+        btnTangcongdong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ConfirmAddress.class);
+                startActivity(intent);
+            }
+        });
+        btnTangnguoingheo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ConfirmAddress.class);
+                startActivity(intent);
+            }
+        });
+        btnTangtuthien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ConfirmAddress.class);
+                startActivity(intent);
+            }
+        });
+        btnQuyengop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ConfirmAddress.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/SmaiApp/ViewHistory.java
+++ app/src/main/java/com/example/SmaiApp/ViewHistory.java
@@ -5,8 +5,18 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.SmaiApp.Adapter.NewsAdapter;
+
+import java.util.ArrayList;
+
+import static com.example.SmaiApp.Helper.Helper.setListViewHeightBasedOnChildren;
 
 public class ViewHistory extends AppCompatActivity {
+
+    ListView lvHistory;
+    ArrayList<News> arrayNews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +29,29 @@ public class ViewHistory extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        //      listview tin đăng mới
+        lvHistory = findViewById(R.id.listViewHistory);
+        arrayNews = new ArrayList<News>();
+
+        arrayNews.add(new News(1, "0971037601", "Học tập", "Dĩ An, Bình Dương", "Sách lớp 1",
+                "Có trà xanh cần bán", "Miễn phí", "23/3/5/2021", R.drawable.pc1));
+        arrayNews.add(new News(1, "0971037601", "Học tập", "Dĩ An, Bình Dương", "Sách lớp 1",
+                "Có trà xanh cần bán", "Miễn phí", "23/3/5/2021", R.drawable.pc1));
+        arrayNews.add(new News(1, "0971037601", "Học tập", "Dĩ An, Bình Dương", "Sách lớp 1",
+                "Có trà xanh cần bán", "Miễn phí", "23/3/5/2021", R.drawable.pc1));
+        arrayNews.add(new News(1, "0971037601", "Học tập", "Dĩ An, Bình Dương", "Sách lớp 1",
+                "Có trà xanh cần bán", "Miễn phí", "23/3/5/2021", R.drawable.pc1));
+        arrayNews.add(new News(1, "0971037601", "Học tập", "Dĩ An, Bình Dương", "Sách lớp 1",
+                "Có trà xanh cần bán", "Miễn phí", "23/3/5/2021", R.drawable.pc1));
+
+        NewsAdapter adapter = new NewsAdapter(
+                this,
+                R.layout.row_news_listview,
+                arrayNews
+        );
+        lvHistory.setAdapter(adapter);
+        setListViewHeightBasedOnChildren(lvHistory);
     }
 }
